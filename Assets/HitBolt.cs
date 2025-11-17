44552e40ea621126a95ce1a839572dfeae63649f
84972 <chuduyloc148205@gmail.com>
2025-11-17 20:48:14 +0700

    sua bolt

---
+++ Assets/HitBolt.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitBolt : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 0.3f);
+    }
+}
